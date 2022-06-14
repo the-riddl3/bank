83671157d19c779db5563bfb05b5bbec826ff0fe
the-riddl3 <v_chelidze2@cu.edu.ge>
2022-06-14 15:13:43 +0400

    transactionProcessor - created transaction fetch, buggy

--- TransactionProcessor/app/Main.hs
+++ TransactionProcessor/app/Main.hs
@@ -1,20 +1,42 @@
+{-# LANGUAGE DeriveGeneric #-}
 
 {-# LANGUAGE OverloadedStrings #-}
-
-import Database.Redis
+{-# LANGUAGE DeriveGeneric #-}
+import Database.Redis hiding (decode)
 import Data.ByteString
+import Data.ByteString.Lazy
+import Data.Text.Encoding
+import System.Exit
+import Data.Either
+import Data.Aeson
 import Control.Monad.Trans
-
 import Data.Maybe (Maybe(Nothing))
+import GHC.Generics
 
-import Lib
+data Transaction = Transaction
+  { senderCardId :: Int,
+    receiverCardId :: Int,
+    amount :: Double
+  }
+  deriving (Show, Generic)
+
+instance FromJSON Transaction
+
+instance ToJSON Transaction
+
 
 myConnectInfo :: ConnectInfo
-myConnectInfo = defaultConnectInfo {connectPort = PortNumber 6380}
+myConnectInfo = defaultConnectInfo {connectPort = PortNumber 6379}
+
 
 main :: IO ()
 main = do
     conn <- connect myConnectInfo
-    runRedis conn $ do
-        currentTransaction <- lpop "list:transactions"
-        liftIO $ print currentTransaction
+    Prelude.putStrLn $ fetchTransactionRedis conn
+
+fetchTransactionRedis :: Connection -> IO (Maybe Transaction)
+fetchTransactionRedis conn = runRedis conn $ do
+  result <- lpop "list:transactions"
+  case result of
+    Right (Just transactionString) -> return $ Just $ decode $ fromStrict transactionString :: Transaction
+    _ -> return Nothing
--- TransactionProcessor/src/Lib.hs
+++ TransactionProcessor/src/Lib.hs
@@ -1,16 +1,2 @@
-{-# LANGUAGE DeriveGeneric #-}
 module Lib where
 
-import Data.Aeson
-import GHC.Generics
-
-data Transaction = Transaction
-  { senderCardId :: Int,
-    receiverCardId :: Int,
-    amount :: Float
-  }
-  deriving (Show, Generic)
-
-instance FromJSON Transaction
-
-instance ToJSON Transaction
